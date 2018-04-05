73ee619218da3cbe9f747cc5f48da84be696c3a1
Poonam <poonamsodani09@gmail.com>
2018-04-05 00:24:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musician,instrument){
   var array = {}
   for(var i=0;i<musician.length;i++){
-    array[i] = musician[i]+'plays'+instrument[i]
+    array[i] = musician[i]+'plays '+instrument[i]
   }
   console.log(array)
   return array
